ccf1f828dde2bbbae24cabb067c27f7b6b0f6f51
Abdul-Rahman Ahmed <abdulrahman102034@gmail.com>
2024-06-26 01:17:54 +0300

    update homepage

--- src/js/header.js
+++ src/js/header.js
@@ -12,3 +12,29 @@ close.addEventListener("click", () => {
   document.querySelector("body").style.overflow = "auto";
 });
 
+let appetizers = [
+  {
+    name: "بروشيتا بالطماطم والريحان",
+    des: "خبز محمص مع زيتون وثوم وطماطم وزيت زيتون.",
+    price: "80 ج.م",
+    src: "/imgs/dishes/Appetizers/Bruschetta.png",
+  },
+  {
+    name: "أرانشيني (كرات الأرز المحشية)",
+    des: "أرز، جبن موزاريلا، بقسماط، زيت للقلي",
+    price: "90 ج.م",
+    src: "/imgs/dishes/Appetizers/Arancini.png",
+  },
+  {
+    name: "كابوناتا",
+    des: "خضروات مشوية مع صلصة الطماطم والثوم والبقدونس.",
+    price: "65 ج.م",
+    src: "/imgs/dishes/Appetizers/Caponata.png",
+  },
+  {
+    name: "أنتيباستو",
+    des: "مجموعة من الخضروات المشوية مع صلصة البيستو.",
+    price: "60 ج.م",
+    src: "/imgs/dishes/Appetizers/Antipasto.png",
+  },
+];
